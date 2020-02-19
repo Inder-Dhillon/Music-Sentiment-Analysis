ba25d91bce4e8b5cbfacc8d11b805862c47626a1
Brendan Taylor <45243880+Brend7n@users.noreply.github.com>
2020-02-19 16:31:59 -0500

    Update musicanalysis.js

--- Front-End/musicanalysis.js
+++ Front-End/musicanalysis.js
@@ -1,8 +1,31 @@
+var mood
 function fun1() {
-    document.getElementById("change").innerHTML="";
-
+    mood = "h"
 }
 
 function fun2() {
-    document.getElementById("change").innerHTML="";
+    mood = "s"
+}
+
+function check(num) {
+    var title;
+
+    switch (num) {
+    case 1: title = "Pop"
+        break;
+    case 2: title = "Rock"
+    break;
+    case 3:title = "Jazz"
+    break;
+    case 4: title = "Metal"
+    break;
+    case 5:title = "Reggae"
+    break;
+    case 6:title = "rap"
+    break;
+    default:
+        break;
+    }
+    var data = $.get("https://api.spotify.com/v1/browse/categories%22,headers=%7B%22Authorization");
+    console.log(data);
 }
